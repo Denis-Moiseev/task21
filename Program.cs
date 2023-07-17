2f918441dffa19b42e27d73a6f1287b39dac977a
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-17 20:01:53 +0800

    finished

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double ShowDistanceTwoPoints3D(int xa, int ya, int za, int xb, int yb, int zb)
+{
+    double AB = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
+    return AB;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int xa = GetInput("Введите координату - xa: ");
+int ya = GetInput("Введите координату - ya: ");
+int za = GetInput("Введите координату - za: ");
+int xb = GetInput("Введите координату - xb: ");
+int yb = GetInput("Введите координату - yb: ");
+int zb = GetInput("Введите координату - zb: ");
+Console.Write($"Расстояние между точками {ShowDistanceTwoPoints3D(xa, ya, za, xb, yb, zb)}");
